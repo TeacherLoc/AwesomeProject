fd4ac5d4f1b37f060acea0dbb11891b134e7260a
TeacherLoc <zZminhrauZz@gmail.com>
2025-04-11 22:53:22 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+import LoginScreen from './components/LoginScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <LoginScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+
+export default App;
