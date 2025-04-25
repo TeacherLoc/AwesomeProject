714b814e7651748e55440c5df4530409aa71ab91
TeacherLoc <zZminhrauZz@gmail.com>
2025-04-25 10:38:17 +0700

    camonTran

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 // filepath: c:\Users\Loocj\Desktop\AwesomeProject\App.tsx
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native'; // Thêm View
-import AppNavigator from './navigation/AppNavigator';
-import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import
+// import AppNavigator from './navigation/AppNavigator';
+// import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import
+import Calculate from './lab1/Calculate';
 
 const App = () => {
   return (
     // Bọc toàn bộ ứng dụng bằng GestureHandlerRootView
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    // <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaView style={styles.container}>
-        <AppNavigator />
+    {/* //     <AppNavigator /> */}
+            <Calculate />
       </SafeAreaView>
-    </GestureHandlerRootView>
+    // </GestureHandlerRootView>
+    
   );
 };
 
